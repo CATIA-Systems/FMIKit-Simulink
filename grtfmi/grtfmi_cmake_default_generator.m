0a048e355fb9191541df5cc8622ee5c5bc2aca1a
Torsten Sommer <torsten.sommer@3ds.com>
2019-06-11 15:51:24 +0200

    Use getenv to retrieve environment variables
    to avoid messages in the command window

--- grtfmi/grtfmi_cmake_default_generator.m
+++ grtfmi/grtfmi_cmake_default_generator.m
@@ -15,15 +15,15 @@ if ispc
     
     if status == 0 && ~isempty(strfind(cmdout, '2017'))  %#ok<STREMP>
         generator = ['Visual Studio 15 2017' platform];
-    elseif system('set VS140COMNTOOLS') == 0
+    elseif ~isempty(getenv('VS140COMNTOOLS'))
         generator = ['Visual Studio 14 2015' platform];
-    elseif system('set VS120COMNTOOLS') == 0
+    elseif ~isempty(getenv('VS120COMNTOOLS'))
         generator = ['Visual Studio 12 2013' platform];
-    elseif system('set VS110COMNTOOLS') == 0
+    elseif ~isempty(getenv('VS110COMNTOOLS'))
         generator = ['Visual Studio 11 2012' platform];
-    elseif system('set VS100COMNTOOLS') == 0
+    elseif ~isempty(getenv('VS100COMNTOOLS'))
         generator = ['Visual Studio 10 2010' platform];
-    elseif system('set VS90COMNTOOLS') == 0
+    elseif ~isempty(getenv('VS90COMNTOOLS'))
         generator = ['Visual Studio 9 2008' platform];
     else
         generator = ['Visual Studio 14 2015' platform];
