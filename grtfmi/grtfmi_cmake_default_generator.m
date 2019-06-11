20966a79db7129387e9d183c819821ec1b53d263
Torsten Sommer <torsten.sommer@3ds.com>
2019-06-11 16:31:38 +0200

    Add support for Visual Studio 2019 in grtfmi.tlc

--- grtfmi/grtfmi_cmake_default_generator.m
+++ grtfmi/grtfmi_cmake_default_generator.m
@@ -7,14 +7,21 @@ if ispc
     else
         platform = '';
     end
+    
+    % use Visual Studio 2015 as default
+    generator = ['Visual Studio 14 2015' platform];
 
     % find the newest installed Visual Studio
     vswhere = [getenv('ProgramFiles(x86)') '\Microsoft Visual Studio\Installer\vswhere.exe'];
     command = ['"' vswhere '" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath'];
     [status, cmdout] = system(command);
     
-    if status == 0 && ~isempty(strfind(cmdout, '2017'))  %#ok<STREMP>
-        generator = ['Visual Studio 15 2017' platform];
+    if status == 0
+        if ~isempty(strfind(cmdout, '2019'))  %#ok<STREMP>
+            generator = 'Visual Studio 16 2019';
+        elseif ~isempty(strfind(cmdout, '2017'))  %#ok<STREMP>
+            generator = ['Visual Studio 15 2017' platform];          
+        end
     elseif ~isempty(getenv('VS140COMNTOOLS'))
         generator = ['Visual Studio 14 2015' platform];
     elseif ~isempty(getenv('VS120COMNTOOLS'))
@@ -25,8 +32,6 @@ if ispc
         generator = ['Visual Studio 10 2010' platform];
     elseif ~isempty(getenv('VS90COMNTOOLS'))
         generator = ['Visual Studio 9 2008' platform];
-    else
-        generator = ['Visual Studio 14 2015' platform];
     end
 else
     generator = 'Unix Makefiles';
