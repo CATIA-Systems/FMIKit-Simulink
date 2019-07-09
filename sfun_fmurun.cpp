61e3e148ccf750e8523f3928fb1b535a93dd3d13
Torsten Sommer <torsten.sommer@3ds.com>
2019-07-09 21:31:29 +0200

    Use default arguments for allocate and freeMemory

--- sfun_fmurun.cpp
+++ sfun_fmurun.cpp
@@ -899,7 +899,7 @@ static void mdlStart(SimStruct *S) {
 	if (fmiVersion(S) == "1.0") {
 
 		if (runAsKind(S) == CO_SIMULATION) {
-			auto slave = new FMU1Slave(guid(S), modelIdentifier(S), unzipDirectory(S), instanceName, calloc, free);
+			auto slave = new FMU1Slave(guid(S), modelIdentifier(S), unzipDirectory(S), instanceName);
             slave->m_userData = S;
             slave->setLogLevel(logLevel(S));
             if (logFMICalls(S)) slave->m_fmiCallLogger = logFMICall;
@@ -908,7 +908,7 @@ static void mdlStart(SimStruct *S) {
 			slave->initializeSlave(time, true, ssGetTFinal(S));
 			p[0] = slave;
 		} else {
-			auto model = new FMU1Model(guid(S), modelIdentifier(S), unzipDirectory(S), instanceName, calloc, free);
+			auto model = new FMU1Model(guid(S), modelIdentifier(S), unzipDirectory(S), instanceName);
             model->m_userData = S;
             model->setLogLevel(logLevel(S));
             if (logFMICalls(S)) model->m_fmiCallLogger = logFMICall;
@@ -925,9 +925,9 @@ static void mdlStart(SimStruct *S) {
 		FMU2 *fmu = nullptr;
 
 		if (runAsKind(S) == CO_SIMULATION) {
-			fmu = new FMU2Slave(guid(S), modelIdentifier(S), unzipDirectory(S), instanceName, calloc, free);
+			fmu = new FMU2Slave(guid(S), modelIdentifier(S), unzipDirectory(S), instanceName);
 		} else {
-			fmu = new FMU2Model(guid(S), modelIdentifier(S), unzipDirectory(S), instanceName, calloc, free);
+			fmu = new FMU2Model(guid(S), modelIdentifier(S), unzipDirectory(S), instanceName);
 		}
 
         fmu->m_userData = S;
