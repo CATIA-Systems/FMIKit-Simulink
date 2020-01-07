0ca46c3d30f6c03b18d5e575ba3fad9299e16ced
Torsten Sommer <torsten.sommer@3ds.com>
2020-01-07 18:43:25 +0100

    Catch exceptions in dumpParameters()
    to handle write-only parameters

--- +FMIKit/dumpParameters.m
+++ +FMIKit/dumpParameters.m
@@ -7,10 +7,12 @@ op = get_param(block, 'ObjectParameters');
 names = fieldnames(op);
 
 for i = 1:numel(names)
+  try
     name = names{i};
     p = get_param(block, name);
     s = matlab.unittest.diagnostics.ConstraintDiagnostic.getDisplayableString(p);
     disp([name repmat(' ', 1, 30 - numel(name)) strtrim(s)])
+  end
 end
 
 end
