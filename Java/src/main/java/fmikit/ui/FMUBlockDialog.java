adf3c8945e8881469270895d1e56e358942f244a
Torsten Sommer <torsten.sommer@3ds.com>
2022-05-23 09:11:57 +0200

    Bump version to 3.0

--- Java/src/main/java/fmikit/ui/FMUBlockDialog.java
+++ Java/src/main/java/fmikit/ui/FMUBlockDialog.java
@@ -78,7 +78,7 @@ public class FMUBlockDialog extends JDialog {
     private JCheckBox resettableCheckBox;
 
     public static boolean debugLogging = false;
-    public static final String FMI_KIT_VERSION = "3.0-beta.1";
+    public static final String FMI_KIT_VERSION = "3.0";
     public static final int MODEL_EXCHANGE = 0;
     public static final int CO_SIMULATION = 1;
     public static HashMap<Double, FMUBlockDialog> dialogs = new HashMap<Double, FMUBlockDialog>();
